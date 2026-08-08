06765c10da2d2e48f665dc42152b232943bbf9bf
viviancubamaja-cyber <englishwithviviam@gmail.com>
2026-08-09 00:43:55 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='mercado-verbos-v36-app';
-const SHELL=['./','./index.html','./app.html','./site.webmanifest','./Ale%20portada%20final.jpeg','./alex-icon-192.png','./alex-icon-512.png'];
+const CACHE='mercado-verbos-v37-app';
+const SHELL=['./','./index.html','./app.html','./site.webmanifest','./Ale%20portada%20final.jpeg','./alex-app-v2-192.png','./alex-app-v2-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});return response;}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));});
