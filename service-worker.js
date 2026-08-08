9fb12f3077bcf613bf22a97ead576f396926a293
viviancubamaja-cyber <viviancubamaja@gmail.com>
2026-08-08 16:08:28 +0100

    Cerrar V34: añadir app instalable con icono de Álex

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='mercado-verbos-v34-app';
+const SHELL=['./','./index.html','./app.html','./site.webmanifest','./alex-icon-192.png','./alex-icon-512.png'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{}));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});return response;}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));});
