56a53634ace79220efacb62929fd675d068f3c3f
viviancubamaja-cyber <englishwithviviam@gmail.com>
2026-08-09 03:09:57 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mercado-verbos-v41-app';
+const CACHE='mercado-verbos-v42-app';
 const SHELL=['./','./index.html','./app.html','./site.webmanifest','./student_identity.js','./Ale%20portada%20final.jpeg','./alex-app-v2-192.png','./alex-app-v2-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
